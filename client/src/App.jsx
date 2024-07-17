232af45e3e568b9d62b45544c7a92d106bed2185
hariom <hk524569@gmail.com>
2024-07-17 21:09:24 +0530

    started fornetend

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+function App() {
+ 
+  return (
+    <>
+      <div className='div1'>
+      <h1 className='text-center'>hariom</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
